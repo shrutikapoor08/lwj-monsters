ea017dcda82a10ac33f1f6f5cc1e19f8860fc2f9
shrutikapoor08 <shruti.kapoor08@gmail.com>
2024-07-23 09:45:51 -0700

    Rendering map in google

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+} from "@vis.gl/react-google-maps";
+const PoiMarkers = (props) => {
+  return (
+    <>
+      {props.pois.map((poi) => (
+        <AdvancedMarker key={poi.key} position={poi.location}>
+          <Pin
+            background={"#FBBC04"}
+            glyphColor={"#000"}
+            borderColor={"#000"}
+          />
+        </AdvancedMarker>
+      ))}
+    </>
+  );
+};
+const locations = [
+  { key: "operaHouse", location: { lat: -33.8567844, lng: 151.213108 } },
+  { key: "tarongaZoo", location: { lat: -33.8472767, lng: 151.2188164 } },
+  { key: "manlyBeach", location: { lat: -33.8209738, lng: 151.2563253 } },
+  { key: "hyderPark", location: { lat: -33.8690081, lng: 151.2052393 } },
+  { key: "theRocks", location: { lat: -33.8587568, lng: 151.2058246 } },
+  { key: "circularQuay", location: { lat: -33.858761, lng: 151.2055688 } },
+  { key: "harbourBridge", location: { lat: -33.852228, lng: 151.2038374 } },
+  { key: "kingsCross", location: { lat: -33.8737375, lng: 151.222569 } },
+  { key: "botanicGardens", location: { lat: -33.864167, lng: 151.216387 } },
+  { key: "museumOfSydney", location: { lat: -33.8636005, lng: 151.2092542 } },
+  { key: "maritimeMuseum", location: { lat: -33.869395, lng: 151.198648 } },
+  { key: "kingStreetWharf", location: { lat: -33.8665445, lng: 151.1989808 } },
+  { key: "aquarium", location: { lat: -33.869627, lng: 151.202146 } },
+  { key: "darlingHarbour", location: { lat: -33.87488, lng: 151.1987113 } },
+  { key: "barangaroo", location: { lat: -33.8605523, lng: 151.1972205 } },
+];
+
+const App = () => {
+  const [currentPosition, setCurrentPosition] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setCurrentPosition({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+    });
+  }, []);
+
+  return (
+    <APIProvider
+      apiKey={"AIzaSyBcixpd3Uc-ssuYJC4Q8HzovlMGpghyVYc"}
+      onLoad={() => console.log("Maps API has loaded.")}
+    >
+      <Map
+        style={{ width: "100vw", height: "100vh" }}
+        defaultCenter={{ lat: 22.54992, lng: 0 }}
+        defaultZoom={3}
+        gestureHandling={"greedy"}
+        disableDefaultUI={true}
+        mapId="DEMO_MAP_ID"
+      >
+        <PoiMarkers pois={locations} />
+        {currentPosition.lat && <AdvancedMarker position={currentPosition} />}
+      </Map>
+    </APIProvider>
+  );
+};
+
+export default App;
